b838a3d6bdb27f3faea64b690d2071fda6b5d55b
v-zmiycharov <valentin.zmiycharov@gmail.com>
2016-05-14 15:19:16 +0300

    Merge test and train businesses, checkins and users

--- src/main/java/utils/Globals.java
+++ src/main/java/utils/Globals.java
@@ -9,53 +9,56 @@ import com.google.gson.GsonBuilder;
 import data.*;
 
 public class Globals {
-	public static List<Business> TRAIN_BUSINESS;
-	public static List<Checkin> TRAIN_CHECKIN;
-	public static List<Review> TRAIN_REVIEW;
-	public static List<User> TRAIN_USER;
+	public static List<Business> BUSINESSES;
+	public static List<Checkin> CHECKINS;
+	public static List<User> USERS;
 
-	public static List<Business> TEST_BUSINESS;
-	public static List<Checkin> TEST_CHECKIN;
-	public static List<Review> TEST_REVIEW;
-	public static List<User> TEST_USER;
+	public static List<Review> TRAIN_REVIEWS;
+
+	public static List<Review> TEST_REVIEWS;
 	
 	public static void init() throws Exception {
 		Gson gson = new Gson();
 		
-		TRAIN_BUSINESS = new ArrayList<Business>();
-		TRAIN_CHECKIN = new ArrayList<Checkin>();
-		TRAIN_REVIEW = new ArrayList<Review>();
-		TRAIN_USER = new ArrayList<User>();
+		BUSINESSES = new ArrayList<Business>();
+		CHECKINS = new ArrayList<Checkin>();
+		USERS = new ArrayList<User>();
 
-		TEST_BUSINESS = new ArrayList<Business>();
-		TEST_CHECKIN = new ArrayList<Checkin>();
-		TEST_REVIEW = new ArrayList<Review>();
-		TEST_USER = new ArrayList<User>();
+		TRAIN_REVIEWS = new ArrayList<Review>();
+		TEST_REVIEWS = new ArrayList<Review>();
 		
+		System.out.println("Read Businesses");
 		for(String line : FileReader.getLines(Constants.TRAIN_BUSINESS_PATH)) {
-			TRAIN_BUSINESS.add(gson.fromJson(line, Business.class));
+			BUSINESSES.add(gson.fromJson(line, Business.class));
 		}
-		for(String line : FileReader.getLines(Constants.TRAIN_CHECKIN_PATH)) {
-			TRAIN_CHECKIN.add(gson.fromJson(line, Checkin.class));
-		}
-		for(String line : FileReader.getLines(Constants.TRAIN_REVIEW_PATH)) {
-			TRAIN_REVIEW.add(gson.fromJson(line, Review.class));
-		}
-		for(String line : FileReader.getLines(Constants.TRAIN_USER_PATH)) {
-			TRAIN_USER.add(gson.fromJson(line, User.class));
+		for(String line : FileReader.getLines(Constants.TEST_BUSINESS_PATH)) {
+			BUSINESSES.add(gson.fromJson(line, Business.class));
 		}
 
-		for(String line : FileReader.getLines(Constants.TEST_BUSINESS_PATH)) {
-			TEST_BUSINESS.add(gson.fromJson(line, Business.class));
+		System.out.println("Read Checkins");
+		for(String line : FileReader.getLines(Constants.TRAIN_CHECKIN_PATH)) {
+			CHECKINS.add(gson.fromJson(line, Checkin.class));
 		}
 		for(String line : FileReader.getLines(Constants.TEST_CHECKIN_PATH)) {
-			TEST_CHECKIN.add(gson.fromJson(line, Checkin.class));
+			CHECKINS.add(gson.fromJson(line, Checkin.class));
 		}
-		for(String line : FileReader.getLines(Constants.TEST_REVIEW_PATH)) {
-			TEST_REVIEW.add(gson.fromJson(line, Review.class));
+
+		System.out.println("Read Users");
+		for(String line : FileReader.getLines(Constants.TRAIN_USER_PATH)) {
+			USERS.add(gson.fromJson(line, User.class));
 		}
 		for(String line : FileReader.getLines(Constants.TEST_USER_PATH)) {
-			TEST_USER.add(gson.fromJson(line, User.class));
+			USERS.add(gson.fromJson(line, User.class));
+		}
+
+		System.out.println("Read Train reviews");
+		for(String line : FileReader.getLines(Constants.TRAIN_REVIEW_PATH)) {
+			TRAIN_REVIEWS.add(gson.fromJson(line, Review.class));
+		}
+
+		System.out.println("Read Test reviews");
+		for(String line : FileReader.getLines(Constants.TEST_REVIEW_PATH)) {
+			TEST_REVIEWS.add(gson.fromJson(line, Review.class));
 		}
 	}
 }
